8d97615029ed654370e436e7c3a1330e72694a41
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2020-11-21 15:21:56 +0100

    Main menu implementation

--- Assets/DiscordAPI/DiscordWrapper.cs
+++ Assets/DiscordAPI/DiscordWrapper.cs
@@ -1,10 +1,98 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-public class DiscordWrapper : MonoBehaviour
+public class DiscordWrapper : Singleton<DiscordWrapper>
 {
-    public void Dupa()
+    #region Fake wrapper
+
+    public string SelectedServer = null;
+    public string SelectedChannel = null;
+
+    public void Logout(Action<bool> resultCallback)
     {
-        var wrapper = new DiscordAPI.Wrapper();
-        GetComponentInChildren<UnityEngine.UI.Text>().text = wrapper.Test().ToString();
+        if (m_connected)
+            StartCoroutine(LogoutRoutine(resultCallback));
+        else
+            resultCallback(true);
     }
+
+    private IEnumerator LogoutRoutine(Action<bool> resultCallback)
+    {
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        SelectedChannel = null;
+        SelectedServer= null;
+
+        m_connected = false;
+        resultCallback(true);
+    }
+
+    public void Connect(Action<bool> resultCallback)
+    {
+        if (m_connected)
+            resultCallback(true);
+        else
+            StartCoroutine(ConnectRoutine(resultCallback));
+    }
+
+    private bool m_connected = false;
+
+    private IEnumerator ConnectRoutine(Action<bool> resultCallback)
+    {
+        yield return new WaitForSecondsRealtime(1.0f);
+        m_connected = true;
+        resultCallback(true);
+    }
+
+    public void GetChannelMessages(DateTime from, DateTime to, Action<Message[]> resultCallback)
+    {
+
+    }
+
+    public void GetServers(Action<string[]> resultCallback)
+    {
+        StartCoroutine(GetServersRoutine(resultCallback));
+    }
+
+    private IEnumerator GetServersRoutine(Action<string[]> resultCallback)
+    {
+        yield return new WaitForSecondsRealtime(1.0f);
+        resultCallback(new[] 
+        {
+            "SamoGenste",
+            "InnyNieWybieraj1",
+            "InnyNieWybieraj2"
+        });
+    }
+
+    public void GetTextChannels(Action<string[]> resultCallback)
+    {
+        StartCoroutine(GetTextChannelsRoutine(resultCallback));
+    }
+
+    private IEnumerator GetTextChannelsRoutine(Action<string[]> resultCallback)
+    {
+        if(string.IsNullOrEmpty(SelectedServer))
+        {
+            resultCallback.Invoke(Array.Empty<string>());
+            yield break;
+        }
+
+        yield return new WaitForSecondsRealtime(1.0f);
+
+        resultCallback.Invoke(new[] 
+        {
+            "general",
+            "linki",
+            "inne"
+        });
+    }
+
+    public struct Message
+    {
+
+    }
+
+    #endregion
 }
---
+++ Assets/MainMenu/SelectChannelButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectChannelButton : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_channelNameText = default;
+    
+    private string m_channelName = default;
+    public string ChannelName
+    {
+        get => m_channelName;
+        set
+        {
+            if (value == m_channelName)
+                return;
+
+            m_channelName = value;
+            m_channelNameText.text = m_channelName;
+        }
+    }
+
+    public static SelectChannelButton Create(Transform root, SelectChannelButton channelPrefab, string channel, Action<string> selectedCallback)
+    {
+        var button = Instantiate(channelPrefab, root);
+        button.ChannelName = channel;
+        button.GetComponentInChildren<Button>().onClick.AddListener(() => selectedCallback.Invoke(channel));
+
+        return button;
+    }
+}
