e95422f7e4eccba8cc15a19dbd101edf8ce18766
Kamil Kowalewski <kamil.kowalewski@engineer.com>
2020-11-21 11:48:03 +0100

    Base app

---
+++ Assets/DiscordAPI/DiscordAPI~/DiscordAPI/DiscordAPI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DiscordAPI
+{
+    public class Wrapper
+    {
+        public int Test() => 10;
+
+        public void Connect(Action<bool> resultCallback)
+        {
+
+        }
+
+        public void GetServers(Action<string[]> resultCallback)
+        {
+
+        }
+
+        public void GetTextChannels(Action<string[]> resultCallback)
+        {
+
+        }
+
+        public void GetChannelMessages(DateTime from, DateTime to, Action<Message[]> resultCallback)
+        {
+
+        }
+    }
+
+    public struct Message
+    {
+        public string Author;
+        public DateTime Timestamp;
+        public string Text;
+    }
+}
---
+++ Assets/DiscordAPI/DiscordWrapper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DiscordWrapper : MonoBehaviour
+{
+    public void Dupa()
+    {
+        var wrapper = new DiscordAPI.Wrapper();
+        GetComponentInChildren<UnityEngine.UI.Text>().text = wrapper.Test().ToString();
+    }
+}
